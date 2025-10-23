850e419f8c2f43cd044a56d8815cd746895a7b21
Ali Ahmed <35622292+AMElashal26@users.noreply.github.com>
2025-10-23 12:17:52 -0400

    Enhance comment clarity in bollinger_band.py

--- bollinger_band.py
+++ bollinger_band.py
@@ -1,3 +1,5 @@
 # Measures price volatility
 
-# The wider the band, the more volatile as they are k-standard deviations from the middle band (period_n SMA)
+# The wider the band, the more volatile the asset prices as they are k-standard deviations from the middle band (period_n SMA)
+#     If 2 standard deviations away, contains 95% of the prices
+# Measures whether a price is too high or too low on a relative basis
