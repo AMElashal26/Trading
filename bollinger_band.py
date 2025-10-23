b0a7065de2abbdf34c4a3aded535df1fcf2d8c72
Ali Ahmed <35622292+AMElashal26@users.noreply.github.com>
2025-10-23 12:18:36 -0400

    Enhance comments for Bollinger Bands explanation
    Added comments to explain the Bollinger Bands concept.

--- bollinger_band.py
+++ bollinger_band.py
@@ -3,3 +3,7 @@
 # The wider the band, the more volatile the asset prices as they are k-standard deviations from the middle band (period_n SMA)
 #     If 2 standard deviations away, contains 95% of the prices
 # Measures whether a price is too high or too low on a relative basis
+
+
+#Relatively high when close to the upper band
+#Relatively cheap when close to the lower band
