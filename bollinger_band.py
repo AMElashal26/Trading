ea0b842a318aedc69088370bbc8a8352070d51e0
Ali Ahmed <35622292+AMElashal26@users.noreply.github.com>
2025-10-23 12:23:01 -0400

    Implement Bollinger Bands visualization
    Added plotting for Bollinger Bands using matplotlib.

--- bollinger_band.py
+++ bollinger_band.py
@@ -7,3 +7,19 @@
 
 #Relatively high when close to the upper band
 #Relatively cheap when close to the lower band
+
+import matplotlib.pyplot as plt
+# Define the Bollinger Bands with 1-sd
+upper_1sd, mid_1sd, lower_1sd = talib.BBANDS(bitcoin_data['Close'], 
+                                     nbdevup=2,
+                                     nbdevdn=2,
+                                     timeperiod=20)
+# Plot the upper and lower Bollinger Bands 
+plt.plot(bitcoin_data['Close'], color='green', label='Price')
+plt.plot(upper_1sd, color='tomato', label="Upper 1sd")
+plt.plot(lower_1sd, color='tomato', label='Lower 1sd')
+
+# Customize and show the plot
+plt.legend(loc='upper left')
+plt.title('Bollinger Bands (1sd)')
+plt.show()
